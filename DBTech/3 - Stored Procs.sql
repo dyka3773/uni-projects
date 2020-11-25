3eda3ba6d9884b3079c24f92e19937e254aa624d
dyka3773 <63119280+dyka3773@users.noreply.github.com>
2020-11-25 23:50:55 +0200

    Stored Procedures added - DB Tech
    I'm not too inventive with ideas about queries for our program, so imma need some help guys

---
+++ DBTech/3 - Stored Procs.sql
@@ -0,0 +1,85 @@
+drop procedure if exists get_Staff;
+drop procedure if exists get_Customers;
+drop procedure if exists get_Books; 
+drop procedure if exists get_StaffWithNameLike;
+drop procedure if exists get_CustomersWithNameLike;
+drop procedure if exists get_BooksWithNameLike;
+drop procedure if exists get_BooksReserevedByThisCustomer;
+drop procedure if exists count_BooksReserevedByThisCustomer;
+drop procedure if exists count_BooksReserevedWithNamesLike;
+
+DELIMITER $$
+create procedure count_BooksReserevedWithNamesLike(
+in onoma varchar(45)
+)
+begin
+	SELECT COUNT(B.name)
+   FROM Books B JOIN Reservations R on (B.bid=R.bid)
+   WHERE B.name = onoma;
+end$$
+
+create procedure get_BooksReserevedByThisCustomer(
+in onoma varchar(45)
+)
+begin
+	SELECT DISTINCT B.name
+   FROM Books B JOIN Reservations R on (B.bid=R.bid)
+	JOIN Customers C on (C.cid=R.cid)
+   WHERE C.name = onoma;
+end$$
+
+create procedure count_BooksReserevedByThisCustomer(
+in onoma varchar(45)
+)
+begin
+	SELECT COUNT(B.name)
+   FROM Books B JOIN Reservations R on (B.bid=R.bid)
+	JOIN Customers C on (C.cid=R.cid)
+   WHERE C.name = onoma;
+end$$
+
+CREATE procedure get_Staff()
+begin
+	SELECT *
+    FROM Staff S;
+end$$
+
+create procedure get_Customers()
+begin
+	SELECT *
+    FROM Customers C;
+end$$
+
+create procedure get_Books()
+begin
+	SELECT *
+    FROM Books B;
+end$$
+
+CREATE procedure get_StaffWithNameLike(
+in onoma varchar(45)
+)
+begin
+	SELECT *
+    FROM Staff S
+    WHERE S.name= onoma;
+end$$
+
+CREATE procedure get_CustomersWithNameLike(
+in onoma varchar(45)
+)
+begin
+	SELECT *
+    FROM Customers C
+    WHERE C.name= onoma;
+end$$
+
+CREATE procedure get_BooksWithNameLike(
+in onoma varchar(45)
+)
+begin
+	SELECT *
+    FROM Books B
+    WHERE B.name= onoma;
+end$$
+delimiter ;
