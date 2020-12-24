18fbe74c9a53696346f8868144aff8d78ce5698f
dyka3773 <63119280+dyka3773@users.noreply.github.com>
2020-12-24 22:16:39 +0200

    Ask4 done - PSP
    I expected it to be longer but python had a library already  :')

--- Parallel Programming/Erg1/Ask4.py
+++ Parallel Programming/Erg1/Ask4.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 24 21:12:20 2020
+import calendar
 
-@author: user
-"""
+yearly_income = int(input("What's you yearly income? "))
+current_year = int(input("What year do you want me to calculate your daily income in? "))
 
+calendar.isleap(current_year)
+
+if (calendar.isleap(current_year)) :
+    daily_income = yearly_income/366
+else:
+    daily_income = yearly_income/365
+
+print("Your daily income is ", daily_income,"€")
