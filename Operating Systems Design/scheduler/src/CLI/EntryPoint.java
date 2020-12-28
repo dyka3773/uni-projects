24ea23a35dbb5aeeed6088192d32ee73bce8ca69
sakis-tzimas <46360819+sakis-tzimas@users.noreply.github.com>
2020-12-28 16:21:00 +0200

    Reverted and corrected some commits.
    Fixed my stupidity.

--- Operating Systems Design/scheduler/src/CLI/EntryPoint.java
+++ Operating Systems Design/scheduler/src/CLI/EntryPoint.java
@@ -1,18 +1,20 @@
 package CLI;
-
 import Scheduler.DiskScheduler.SCAN;
+import Scheduler.ProcessScheduler.PriorityQueue;
 import Scheduler.SimProcess;
 import java.util.ArrayList;
 
 public class EntryPoint {
     public static void main(String[] args) {
-
-        ArrayList<SimProcess> sequence = new ArrayList();
+        
+        //Create memory
+        ArrayList<SimProcess> memory = new ArrayList();
+        
+        //Create some processes
         for(int i=1; i<101; i++)
-            sequence.add(new SimProcess("P"+i));
-        
-        
-        SCAN s = new SCAN(sequence, SCAN.Direction.LEFT);
-        System.out.println(sequence.toString()+'\n'+s.toString());
+            memory.add(new SimProcess("P"+i));
+        PriorityQueue pq = new PriorityQueue(memory);
+        pq.start();
+        pq.printProcessQueues();
     }
 }
--- Operating Systems Design/scheduler/src/Scheduler/SimProcess.java
+++ Operating Systems Design/scheduler/src/Scheduler/SimProcess.java
@@ -10,7 +10,9 @@ public class SimProcess {
     private int priority;
     private long size; // Size in bytes
     private long trackAddress;
+    private String trackAddressHex = "0x0";
     private boolean needsIO = false;
+    public static final int PRIORITIES = 5;
 
     public SimProcess(String name, int arrivalTime, int burstTime, int priority,
             long size, long trackAddress) {
@@ -26,7 +28,7 @@ public class SimProcess {
         this.name = name;
         this.arrivalTime = (int)(Math.random()* 50);
         this.burstTime = (int)(Math.random()* 50);
-        this.priority = (int)(Math.random()* 5);
+        this.priority = (int)(Math.random()* SimProcess.PRIORITIES);
         this.size = (long)(Math.random()* SCAN.MBYTES);
         this.trackAddress = (int)(Math.random()* SCAN.DISK_SIZE);
         this.needsIO = new Random().nextBoolean();
@@ -55,12 +57,23 @@ public class SimProcess {
     public long getTrackAddress() {
         return trackAddress;
     }
+    
+    public String getTrackAddressHex() {
+        return (trackAddressHex + Integer.toHexString((int)trackAddress));
+    }
+    
+    public boolean needsIO() {
+        return needsIO;
+    }
 
     @Override
     public String toString() {
         return "\nProcess:" + "\n\tPID: " + name + "\n\tArrival Time: " + arrivalTime 
                 + "\n\tBurst Time: " + burstTime + " ms\n\tPriority: " + priority 
                 + "\n\tSize: " + size + " Bytes"
-                + "\n\tDisk Address: " + trackAddress + '\n';
+                + "\n\tDisk Address: " + trackAddress
+                + "\n\tDisk Address (in Hex): " + trackAddressHex 
+                + Integer.toHexString((int)trackAddress)
+                + "\n\tNeeds IO: " + needsIO + '\n';
     }
 }
