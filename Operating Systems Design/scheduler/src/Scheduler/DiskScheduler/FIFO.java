a4ac8dcc3c5f59119b9348245475c89bcd85b5cd
dyka3773 <63119280+dyka3773@users.noreply.github.com>
2021-01-03 00:34:47 +0200

    FIFO done - OSD

--- Operating Systems Design/scheduler/src/Scheduler/DiskScheduler/FIFO.java
+++ Operating Systems Design/scheduler/src/Scheduler/DiskScheduler/FIFO.java
@@ -2,45 +2,61 @@ package Scheduler.DiskScheduler;
 import Scheduler.SimProcess;
 import java.util.ArrayList;
 
-public class FIFO {
+public class FIFO implements DiskScheduler{
     
     private ArrayList<SimProcess> requestQueue;
-    private long currentTrack = 100;//(long) (Math.random() * 200 + 1);
+    private long currentTrack;//(long) (Math.random() * 200 + 1);
     private double avgSeekLength = 0;
     private long tracksTraversedSum;
+    private ArrayList<SimProcess> processTrackSequence;
     
     public FIFO(ArrayList<SimProcess> ProcessList) {
         requestQueue = ProcessList;
-        SimProcessSort(requestQueue);
-        tracksTraversedSum = Math.abs(requestQueue.get(0).getTrackAddress() - currentTrack);
-        currentTrack = requestQueue.get(0).getTrackAddress();
-        
-        for (int i = 1; i < requestQueue.size(); i++){         
-            tracksTraversedSum += Math.abs(requestQueue.get(i).getTrackAddress() - currentTrack);
-            currentTrack = requestQueue.get(i).getTrackAddress();
-        }
-        
-        avgSeekLength =  tracksTraversedSum / (double) (requestQueue.size());       
-    }
-    
-    private void SimProcessSort(ArrayList<SimProcess> ProcessList){
-        SimProcess temp;
-        int length = ProcessList.size();
-        
-        for (int i = 0; i < length; i++){
-            for (int j = 1; j < (length - i); j++){
-                if (ProcessList.get(j - 1).getArrivalTime() > ProcessList.get(j).getArrivalTime()){
-                    temp = ProcessList.get(j - 1);
-                    ProcessList.set(j - 1, ProcessList.get(j));
-                    ProcessList.set(j, temp);
-                }
-            }
-        } 
     }
     
     @Override
     public String toString(){
         return "FIFO: Average seek length is " + avgSeekLength;
+    }
+
+    @Override
+    public void start() {
+        tracksTraversedSum = Math.abs(requestQueue.get(0).getTrackAddress() - currentTrack);
+        currentTrack = requestQueue.get(0).getTrackAddress();
+        processTrackSequence.add(requestQueue.get(0));
+        
+        for (int i = 1; i < requestQueue.size(); i++){         
+            tracksTraversedSum += Math.abs(requestQueue.get(i).getTrackAddress() - currentTrack);
+            currentTrack = requestQueue.get(i).getTrackAddress();
+            processTrackSequence.add(requestQueue.get(i));
+        }
+        
+        avgSeekLength =  tracksTraversedSum / (double) (requestQueue.size()); 
+    }
+
+    @Override
+    public String getHeadPosition() {
+        return Long.toString(currentTrack);
+    }
+
+    @Override
+    public void setHeadPosition(long hpos) {
+        currentTrack = hpos;
+    }
+
+    @Override
+    public String getProcessTrackSequence() {
+        return processTrackSequence.toString();
+    }
+
+    @Override
+    public String getSeekOperations() {
+        return Long.toString(tracksTraversedSum);
+    }
+
+    @Override
+    public void setSeekOperations(long sop) {
+        tracksTraversedSum = sop;
     }
     
 }
