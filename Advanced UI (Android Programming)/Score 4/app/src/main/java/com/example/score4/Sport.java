39164c476649747ca5abc7900ac9b37af0cf07f2
dyka3773 <63119280+dyka3773@users.noreply.github.com>
2021-05-01 11:29:13 +0300

    Deleted unnecessary lines and comments for now + Ready to plug in the event listeners in each fragment - AUI

--- Advanced UI (Android Programming)/Score 4/app/src/main/java/com/example/score4/Sport.java
+++ Advanced UI (Android Programming)/Score 4/app/src/main/java/com/example/score4/Sport.java
@@ -10,22 +10,14 @@ import android.view.ViewGroup;
 
 public class Sport extends Fragment {
 
-
-    public Sport() {
-        // Required empty public constructor
-    }
-
-    //CAN WE MAKE THIS LOAD UP DYNAMICALLY TOO???
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.sport_fragment, container, false);
+        View view = inflater.inflate(R.layout.sport_fragment, container, false);
+
+
+
+        return view;
     }
 }
